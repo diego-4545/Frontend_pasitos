ddcd197ac323275f7d154b8f7f0604ee3c5e3830
Diego <diego.escamillat@uanl.edu.mx>
2026-02-09 20:56:53 -0600

    Interfaces vacias admin y maestro

--- app/src/main/java/com/example/pasitos/AdminActivity.kt
+++ app/src/main/java/com/example/pasitos/AdminActivity.kt
@@ -1,34 +1,20 @@
 package com.example.pasitos
 
-import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+class AdminActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-
+        setContentView(R.layout.activity_admin)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
-        }
-
-        val btnAdmin = findViewById<Button>(R.id.btnAdmin)
-        val btnMaestros = findViewById<Button>(R.id.btnMaestros)
-
-        btnAdmin.setOnClickListener {
-            startActivity(Intent(this, AdminActivity::class.java))
-        }
-
-        btnMaestros.setOnClickListener {
-            startActivity(Intent(this, MaestroActivity::class.java))
         }
     }
 }
