90d9a947ba68c40c1e6b2a8d04bcacebc33201b5
Diego <diego.escamillat@uanl.edu.mx>
2026-02-11 14:38:57 -0600

    Mas interfaces en blanco

---
+++ app/src/main/java/com/example/pasitos/SalidasActivity.kt
@@ -0,0 +1,20 @@
+package com.example.pasitos
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class SalidasActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_salidas)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
